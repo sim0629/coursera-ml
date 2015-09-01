df311857a0b4626c64257c84b91654e6df771628
Gyumin Sim <sim0629@gmail.com>
2015-09-01 14:48:57 +0900

    gradient descent

--- gradientDescent.m
+++ gradientDescent.m
@@ -17,11 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    diff = X * theta - y
+    delta = alpha / m * sum(X .* [diff, diff])
+    theta = theta - delta'
 
     % ============================================================
 
